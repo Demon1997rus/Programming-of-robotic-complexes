063af79ea7e83282fd982b626b6781f697b030d3
Demon1997rus <rotanov.1997@mail.ru>
2025-03-24 20:47:08 +0300

    Реализация 1 лабы по 10 варианту - Движение по логарифмической траектории (a и b – точки начала и конца движения)

--- lab_first/src/start_turtle.py
+++ lab_first/src/start_turtle.py
@@ -2,31 +2,53 @@
 
 import rospy
 import sys
+import numpy as np
 from geometry_msgs.msg import Twist
 
 print(sys.version)
 
-
-class GoForward():
-    def __init__(self):
+class GoLogarithmicTrajectory():
+    def __init__(self, a, b):
         rospy.init_node('turtlesim_script', anonymous=False)
         rospy.loginfo("To stop Turtle CTRL + C")
 
         rospy.on_shutdown(self.shutdown)
         self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
-        r = rospy.Rate(2)
-        i = 1
-        while not rospy.is_shutdown():
-            rospy.loginfo(i)
+        self.turn_left(83)
 
+        r = rospy.Rate(10)
+        t = 0.0
+        while not rospy.is_shutdown() and t < 1.0:
             move_cmd = Twist()
-            move_cmd.linear.x = 1
-            move_cmd.linear.y = 0
-            move_cmd.angular.z = 10 / i
+            # Параметризация траектории
+            x = a + (b - a) * t
+            y = np.log(x)
+            
+            # Вычисление скорости
+            dx_dt = (b - a)
+            dy_dt = 1 / x * dx_dt
+            
+            # Линейная скорость
+            move_cmd.linear.x = np.sqrt(dx_dt**2 + dy_dt**2)
+            
+            # Угловая скорость
+            move_cmd.angular.z = np.arctan2(dy_dt, dx_dt)
+            
             self.cmd_vel.publish(move_cmd)
+            rospy.loginfo(f"X: {x}, Y: {y}, Linear: {move_cmd.linear.x}, Angular: {move_cmd.angular.z}")
             r.sleep()
-            i += 1
+            t += 0.08  # Увеличиваем параметр t
+
+    def turn_left(self, angle):
+        """Поворачивает черепашку на заданный угол влево."""
+        turn_cmd = Twist()
+        turn_cmd.angular.z = np.radians(angle)  # Преобразуем градусы в радианы
+        start_time = rospy.Time.now()
+        duration = rospy.Duration(1)  # Задаем длительность поворота
+        while rospy.Time.now() - start_time < duration:
+            self.cmd_vel.publish(turn_cmd)
+            rospy.sleep(0.1)
 
     def shutdown(self):
         # stop turtlebot
@@ -36,10 +58,10 @@ class GoForward():
         # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
 
-
 if __name__ == '__main__':
     try:
-        GoForward()
+        a, b = 1, 5
+        GoLogarithmicTrajectory(a, b)
     except:
         rospy.loginfo("GoForward node terminated.")
         rospy.loginfo(sys.exc_info()[:2])
